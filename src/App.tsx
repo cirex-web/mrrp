455c71fbc0aabae70081df99bb98848876d93da4
topshenyi-web <erxu@ctemc.org>
2025-12-03 18:19:39 -0500

    fix: sizing

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div>I love impulse purchases :3</div>;
+  return <div className="yawn">I love impulse purchases :3</div>;
 }
 
 export default App;
